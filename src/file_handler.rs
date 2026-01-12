1f98d0b008bd818418d65bcbcdf6ad0d0b44ceaf
Elwin Jyothis <elwin.jyothis@fintechglobal.center>
2026-01-12 15:32:51 +0530

    feat: workspace discovery & auto execution

---
+++ src/file_handler.rs
@@ -0,0 +1,63 @@
+use std::{env::current_dir, ffi::OsStr, fs, path::Path};
+
+use walkdir::WalkDir;
+
+pub fn read_queries_from_file(path: &str) -> Result<String, String> {
+    let path = Path::new(path);
+    if path.extension().unwrap_or(OsStr::new("")) != "hop" {
+        return Err("Only .hop files are supported!".into());
+    }
+
+    if !path.exists() {
+        return Err(format!("{} no such file or directory", path.to_string_lossy()));
+    }
+
+    let queries = fs::read_to_string(path).map_err(|e| format!("Could not read the passed file, {}", e))?;
+    Ok(queries)
+}
+
+pub fn read_queries_from_workspace() -> Result<String, String> {
+    let mut path = current_dir().map_err(|_| "Failed to get current working directory")?;
+    path.push(".nethop");
+    
+    let config_path = path.join("config.hop");
+    if !config_path.exists() {
+        return Err("Your project does not have nethop setup. Run nethop init to get a basic structure initialized".to_string());
+    }
+
+    let mut script = String::new();
+    let mut files_found = 0; 
+
+    let headers = fs::read_to_string(&config_path)
+        .map_err(|_| "Failed to read base config.hop file")?;
+    script.push_str(&headers);
+    script.push('\n');
+
+    println!("🔍 Searching workspace files...\n");
+
+    for entry in WalkDir::new(&path).into_iter().filter_map(|et| et.ok()) {
+        let file_path = entry.path();
+        
+        if file_path.is_file() 
+           && file_path.extension().map_or(false, |f| f == "hop") 
+           && file_path != config_path 
+        {
+            let content = fs::read_to_string(file_path)
+                .map_err(|e| format!("Failed to read {}: {}", file_path.display(), e))?;
+            
+            script.push_str(&content);
+            script.push('\n');
+
+            files_found += 1;
+            println!("🕸️  {} file added", file_path.file_name().unwrap_or(OsStr::new("")).to_string_lossy());
+        }
+    }
+
+    if files_found == 0 {
+        println!("\n⚠️  Only config.hop was found.\n");
+    } else {
+        println!("\n✅ {} additional hops found. \n", files_found);
+    }
+
+    Ok(script)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,40 @@ mod hop_lang;
 mod http;
 mod network;
 mod ui;
+mod file_handler;
 
-use std::error::Error;
-use std::fs;
+use std::{env, error::Error, io::{self, Write}};
 
 use crate::{
     hop_lang::{clean_script, fetch_connection_header, fetch_requests},
     network::connect,
     network::execute_batch_requests,
+    file_handler::{read_queries_from_file, read_queries_from_workspace}
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: Organise the file handling
-    // to a separate module
-    let query_raw = fs::read_to_string("example.hop")?;
+    println!("🐇 NetHop v0.1-Beta");
+
+    let args: Vec<String> = env::args().collect();
+    let query_raw = match args.get(1) {
+        Some(file_path) => read_queries_from_file(file_path)?,
+        None => read_queries_from_workspace()?
+    };
 
     let cleaned_queries = clean_script(&query_raw)?;
-    let mut conn = fetch_connection_header(&cleaned_queries)?;
-    connect(&mut conn)?;
+    let mut start_query = String::new();
+    print!("Queries prepared, start execution? [Y/n]: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut start_query)?;
 
-    let all_requests = fetch_requests(&cleaned_queries)?;
-
-    execute_batch_requests(all_requests, &mut conn)?;
+    if start_query.trim().to_lowercase() == "y" {
+        let mut conn = fetch_connection_header(&cleaned_queries)?;
+        connect(&mut conn)?;
+        let all_requests = fetch_requests(&cleaned_queries)?;
+        execute_batch_requests(all_requests, &mut conn)?;
+    } else {
+        println!("❌ Cancelled");
+    }
+    
     Ok(())
 }
